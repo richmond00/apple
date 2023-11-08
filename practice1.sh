b53377f4014a57bd98118ddabc310afec66603d4
Dongwook Kim <dongwook227@gmail.com>
2023-11-08 08:03:50 -0500

    ㅁㅇㅇㄷ

---
+++ practice1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Exercise 1
+echo "Shell Scripting is Fun!"
+
+# Exercise 2
+MY_VARIABLE="Kim"
+echo "Shell Scripting is Fun! ${MY_VARIABLE}"
+
+# Exercise 3
+HOSTNAME=(hostname)
+echo "This script is running on ${HOSTNAME}"
